765b472fcf92097afef92aacaafcd6961b9dc1f4
laurentbedu <bedulaurent@gmail.com>
2024-07-30 06:33:43 +0200

    Step07

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Branch Step03 : Composant ArticleCard, useEffect hook and conditional rendering<
 Branch Step04 : Composant ArticleScreen, data mapping and propTypes<br />
 Branch Step05 : Composants Génériques et props children<br />
 Branch Step06 : Composants Génériques et components as props<br />
+Branch Step07 : Formulaire de contact non contrôlé<br />
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,27 @@
 function Contact() {
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const jsonData = Object.fromEntries(formData.entries());
+        console.log(jsonData);
+        const submitData = async () => {
+            const url = "http://api.php-blog-project.loc/contact";
+            const options = {
+                method: "POST",
+                body: JSON.stringify(jsonData),
+            }
+            const response = await fetch(url, options);
+            console.log(response);
+            if (!response.ok) {
+                throw new Error('Erreur de réseau');
+            }
+            const json = await response.json();
+            console.log(json.result);
+        }
+        submitData();
+    };
+
     return ( 
         <div className="modal fade" id="myModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
@@ -8,22 +31,28 @@ function Contact() {
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        <form id="contactForm" method="POST" action="">
+                        <form id="contactForm" onSubmit={handleFormSubmit}>
                             <div className="mb-3">
                                 <label className="form-label" htmlFor="fullname">Name</label>
-                                <input className="form-control" name="fullname" type="text" placeholder="Name" />
+                                <input 
+                                    className="form-control" name="fullname" type="text" placeholder="Name" />
                             </div>
                             <div className="mb-3">
                                 <label className="form-label" htmlFor="email">Email Address</label>
-                                <input className="form-control" name="email" type="email" placeholder="Email Address" />
+                                <input 
+                                    className="form-control" name="email" type="email" placeholder="Email Address" />
                             </div>
                             <div className="mb-3">
                                 <label className="form-label" htmlFor="message">Message</label>
-                                <textarea className="form-control" name="message" type="text" placeholder="Message"
-                                    style={{height: '10rem'}}></textarea>
+                                <textarea 
+                                    className="form-control" name="message" type="text"
+                                    placeholder="Message" style={{height: '10rem'}} >
+                                </textarea>
                             </div>
                             <div className="mb-1 text-center">
-                                <button className="btn btn-success" name="send" type="submit">Envoyer</button>
+                                <button className="btn btn-success" name="send" type="submit">
+                                    Envoyer
+                                </button>
                             </div>
                         </form>
                     </div>
